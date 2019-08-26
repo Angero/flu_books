1f528aa53a8f325212cd881cd9db8225d770f35c
RA <ra.perm.russia@gmail.com>
2019-08-26 17:30:56 +0500

    0.1.7

--- lib/page_books.dart
+++ lib/page_books.dart
@@ -47,19 +47,63 @@ class _BooksPageState extends State<BooksPage> {
     return InkWell(
       child: Container(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
+        child: MediaQuery.of(context).orientation == Orientation.portrait
+            ? _portraitWidget(book)
+            : _landscapeWidget(book),
+      ),
+      onTap: () {
+        Books.book = book;
+        Navigator.of(context).pushNamed('/book');
+      },
+    );
+  }
+
+  Widget _portraitWidget(Book book) {
+    return Column(
+      children: <Widget>[
+        Container(
+          width: 126.0,
+          height: 203.0,
+          color: Colors.black12,
+          child: book.image == null || book.image == ''
+              ? Container()
+              : Image.network(book.image),
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Text(
+          book.name,
+          style: TextStyle(fontSize: 22.0),
+        ),
+        SizedBox(
+          height: 5.0,
+        ),
+        Text(
+          book.author,
+          style: TextStyle(fontSize: 16.0),
+        ),
+      ],
+    );
+  }
+
+  Widget _landscapeWidget(Book book) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: 126.0,
+          height: 203.0,
+          color: Colors.black12,
+          child: book.image == null || book.image == ''
+              ? Container()
+              : Image.network(book.image),
+        ),
+        Expanded(child: Container(), flex: 1,),
+//        SizedBox(
+//          width: 10.0,
+//        ),
+        Column(
           children: <Widget>[
-            Container(
-              width: 126.0,
-              height: 203.0,
-              color: Colors.black12,
-              child: book.image == null || book.image == ''
-                  ? Container()
-                  : Image.network(book.image),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
             Text(
               book.name,
               style: TextStyle(fontSize: 22.0),
@@ -73,11 +117,9 @@ class _BooksPageState extends State<BooksPage> {
             ),
           ],
         ),
-      ),
-      onTap: () {
-        Books.book = book;
-        Navigator.of(context).pushNamed('/book');
-      },
+        Expanded(child: Container(), flex: 1,)
+
+      ],
     );
   }
 }
